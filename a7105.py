02d0e33187a1464b81d02dc29f267c1447415df3
Patrick Chilton <chpatrick@gmail.com>
2014-02-05 18:14:22 +0100

    Refactoring.

--- a7105.py
+++ a7105.py
@@ -2,35 +2,63 @@ from mpsse import *
 import time
 from struct import *
 
-REG_GIO1S = 0x0b
+class Reg:
+  MODE  = 0x00 # reset, etc
+  ID    = 0x06 # used to set transmitter ID
+  GIO1S = 0x0b # enables 4-wire SPI
 
-CONST_4WIRE = 0x19
+READ_BIT = 0x40 # flag bit specifying register should be read
+
+ENABLE_4WIRE = 0x19 # value written to GIO1S to enable 4-wire SPI
+
+# context guard for SPI
+class SPIContext:
+  def __init__(self, spi):
+    self.spi = spi
+
+  def __enter__(self):
+    self.spi.Start()
+
+  def __exit__(self, type, value, traceback):
+    self.spi.Stop()
+    return False
+
+# pack a byte
+def pbyte(byte):
+  return pack('B', byte)
+
+# unpack a byte
+def ubyte(bytestring):
+  return unpack('B', bytestring)[0]
 
 class A7105:
   def init(self):
     self.spi = MPSSE(SPI0, TEN_MHZ, MSB)
-    self.write_reg(REG_GIO1S, CONST_4WIRE)
+    self.spi_on = SPIContext(self.spi)
+    self.write_reg(Reg.GIO1S, ENABLE_4WIRE)
 
   def write_reg(self, reg, value):
-    self.spi.Start()
-    self.spi.Write(pack('BB', reg, value))
-    self.spi.Stop()
+    with self.spi_on:
+      self.spi.Write(pack('BB', reg, value))
 
   def read_reg(self, reg):
-    self.spi.Start()
-    self.spi.Write(pack('B', 0x40 | reg))
-    value = unpack('B', self.spi.Read(1))[0]
-    self.spi.Stop()
+    value = None
+    with self.spi_on:
+      self.spi.Write(pbyte(READ_BIT | reg))
+      value = ubyte(self.spi.Read(1))
     return value
 
   def reset(self):
-    self.write_reg(0, 0)
+    self.write_reg(Reg.MODE, 0x00)
+
+  def write_id(self, id):
+    with self.spi_on:
+      self.spi.Write(pbyte(Reg.ID) + id)
 
 a7105 = A7105()
 a7105.init()
 time.sleep(1)
-time.sleep(1)
 for r in xrange(256):
   val = a7105.read_reg(r)
-  print r, val
-  time.sleep(0.2)
+  print r, bin(val)
+  time.sleep(0.05)
