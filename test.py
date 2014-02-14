ed1bfac80342b75a88955fa92f8e906965b59cbf
Patrick Chilton <chpatrick@gmail.com>
2014-02-14 15:10:48 +0100

    Add sine-throttling test.

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from hubsan import *
 import logging
+import math
 
 logging.basicConfig(level = logging.INFO)
 
@@ -7,5 +8,14 @@ hubsan = Hubsan()
 hubsan.init()
 hubsan.bind()
 time.sleep(2)
+
+for i in xrange(100):
+  hubsan.control(0x00, 0x80, 0x7d, 0x84)
+
+print "throttling up"
+
+x = 0
+
 while True:
-  hubsan.control(0xe0, 0x80, 0x7d, 0x84)
+  x += 0.05
+  hubsan.control(int((math.sin(x) + 1) * 127.5), 0x80, 0x7d, 0x84)
